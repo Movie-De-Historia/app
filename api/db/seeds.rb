fa2d6c16c5f3ac4b025f6fe91cf476970a759df8
TakahiroSono <taka.sono889@gmail.com>
2019-10-04 22:05:06 +0900

    #21 モック用データ作成完了

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -7,18 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "csv"
 
-5.times do |no|
-  User.create(name:"user_#{no}",email:"user_#{no}@mail.com",password:"password",password_confirmation:"password")
+users = 5.times.map do |no|
+  User.create!(name:"user_#{no}",email:"user_#{no}@mail.com",password:"password",password_confirmation:"password")
 end
 
-CSV.foreach('db/review_seed.csv') do |row|
-  Review.create(:genre_id => row[0], :user_id => row[1], :movie_title => row[2], :head_text => row[3], :comment => row[4], :spoiler => row[5])
+genres = CSV.foreach('db/genre_seed.csv').map do |row|
+  Genre.create!(:name => row[0])
 end
 
-CSV.foreach('db/genre_seed.csv') do |row|
-  Genre.create(:name => row[0])
+reviews = CSV.foreach('db/review_seed.csv').map do |row|
+  genre = genres[row[0].to_i - 1]
+  user = users[row[1].to_i - 1]
+  Review.create!(:genre_id => genre.id, :user_id => user.id, :movie_title => row[2], :head_text => row[3], :comment => row[4], :spoiler => row[5])
 end
 
 CSV.foreach('db/log_seed.csv') do |row|
-  Log.create(:user_id => row[0], :review_id => row[1], :look_at => Time.now, :like => row[2])
+  user = users[row[0].to_i - 1 ]
+  review = reviews[row[1].to_i - 1 ]
+  Log.create!(:user_id => user.id, :review_id => review.id, :look_at => Time.now, :like => row[2])
 end
