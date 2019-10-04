5a70cf3e5d6451db941021d5ece0f0d2d426c77e
TakahiroSono <taka.sono889@gmail.com>
2019-10-04 18:56:02 +0900

    モック用データ作成 #21

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+5.times do |no|
+  User.create(name:"user_#{no}",email:"user_#{no}@mail.com",password:"password",password_confirmation:"password")
+end
+
+CSV.foreach('db/review_seed.csv') do |row|
+  Review.create(:genre_id => row[0], :user_id => row[1], :movie_title => row[2], :head_text => row[3], :comment => row[4], :spoiler => row[5])
+end
+
+CSV.foreach('db/genre_seed.csv') do |row|
+  Genre.create(:name => row[0])
+end
+
+CSV.foreach('db/log_seed.csv') do |row|
+  Log.create(:user_id => row[0], :review_id => row[1], :look_at => Time.now, :like => row[2])
+end
