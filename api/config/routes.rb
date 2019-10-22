60208d75bff2dc7022c6f18b1d29d3489fce232a
takanoshuhei <syu02030203@gmail.com>
2019-10-22 10:27:31 +0900

    Update routes.rb

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  post '/signin', to: 'sessions#sign_in'
+  delete '/logout', to: 'sessions#destroy'
+
   get  'genre/index',  to: 'genres#index'
   get  'genre/show/:id',  to: 'genres#show'
+  
   get  'log',  to: 'logs#index'
   get  'log/:id',  to: 'logs#show'
   post 'log',  to: 'logs#create'
+
+  # resources :users
+  get '/me', to: 'users#show'
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
+  put '/users', to: 'users#update'
+  delete '/users', to: 'users#destroy'
+  
   resources :users
 end
 
