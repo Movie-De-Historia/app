815bf22c361c7bf80aaf9b4a0ebd56cf96bea8b5
tsuno0829 <tsnry7913@gmail.com>
2019-10-22 13:13:59 +0900

    rootReducer化できていなかったので変更

--- front/src/modules/rootState.js
+++ front/src/modules/rootState.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux-starter-kit";
 import movieCommentsModule from "./movieCommentsModule";
 
-export const rootReducer = combineReducers({
-    movieComments: movieCommentsModule,
+const rootReducer = combineReducers({
+    movieComments: movieCommentsModule.reducer,
 });
+
+export default rootReducer;
