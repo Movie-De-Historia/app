501a8c5c0e4d9c2942615c6e1a022412cc17f64f
tsuno0829 <tsnry7913@gmail.com>
2019-11-09 10:51:07 +0900

    途中経過を保存

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -7,6 +7,10 @@ import { useDispatch } from "react-redux";
 import movieCommentsModule, { useMovieComments } from "../../modules/movieCommentsModule";
 
 const Header = ( props ) => {
+    const [flag, setFlag] = useState(true);
+    const LogoReturn = props.displayLogoReturn ? <img src={logo_return} className="logo-return" alt="logo" /> : <></>;
+    const DisplayColor = props.displayColor === "gray" ? "gray" : "black";
+    const MyPageLogo = props.MyPageLogo ? <img src={human} className={"Header-logo-" + DisplayColor} alt="logo" /> : <></>;
     let title = props.title;
     const date = new Date();
     const dispatch = useDispatch();
@@ -22,32 +26,33 @@ const Header = ( props ) => {
     };
     let lifeTime;
     const limitTime = 24;
-    const diffTime = limitTime - Math.floor((date.getTime() - savedTime ) / (1000 * 60));
-    const [time, setTime] = useState(diffTime < 0 ? 0 : diffTime);
+    const latestDate = new Date();
+    const elapsedTime = limitTime - Math.floor(( latestDate.getTime() - savedTime ) / (1000 * 60));
+    const [time, setTime] = useState(24);
+    console.log(useMovieComments()["movieComments"]);
+    // if (flag === props.LifeTime) {
+        // console.log(latestDate.getTime(), date.getTime());
+        // console.log(elapsedTime);
+        // setFlag(false);
+        // setTime(limitTime);
+    // };
 
     const useTimer = (currentDate) => {
         useEffect(() => {
             let timerID = 0;
-            if ( (props.LifeTime === true) ) {
+            if (props.LifeTime) {
                 // 初めて保存したときの時間をreduxに一度だけ追加
                 // saveTime(currentDate.getTime());
-
                 if (time <= 0) {
                     clearInterval(timerID);
                 } else {
                     timerID = setInterval(tick(), 1000);
                 }
             }
-
             return () => clearInterval(timerID);
         }, [time]);
 
         const tick = () => {
-            // デバックしやすいように「時」ではなく「分」で計算（あとで戻す）
-            // ミリ秒を分に変換して計算するため1000x60で割る
-            const latestDate = new Date();
-            const getTime = latestDate.getTime();
-            const elapsedTime = limitTime - Math.floor(( getTime - savedTime ) / (1000 * 60));
             // デバック用（後で消す）
             console.log(`残り時間: ${elapsedTime} 分`)
             // 一定時間ごとに更新
@@ -58,9 +63,6 @@ const Header = ( props ) => {
     };
 
     const timer = useTimer(new Date());
-    const LogoReturn = props.displayLogoReturn ? <img src={logo_return} className="logo-return" alt="logo" /> : <></>;
-    const DisplayColor = props.displayColor === "gray" ? "gray" : "black";
-    const MyPageLogo = props.MyPageLogo ? <img src={human} className={"Header-logo-" + DisplayColor} alt="logo" /> : <></>;
 
     if (props.LifeTime) {
         lifeTime =
