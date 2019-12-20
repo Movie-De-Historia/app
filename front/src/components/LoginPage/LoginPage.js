7c4bc8f04bc7d159d0f53d37455cc2bbeefa5293
tsuno0829 <tsnry7913@gmail.com>
2019-12-20 15:16:11 +0900

    LoginPageからReceivedListに移動できるようにした

--- front/src/components/LoginPage/LoginPage.js
+++ front/src/components/LoginPage/LoginPage.js
@@ -1,11 +1,32 @@
 import React from "react";
 import historia from "../../image/historia.svg";
+import './LoginPage.scss'
+import Fab from '@material-ui/core/Fab';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
     return(
         <>
+            <div className="back_ground"></div>
             <img src={historia} className="historia" alt="historia-logo" />
+            <div className="login_form"></div>
+            <div className="mail_address">メールアドレス</div>
+            <form>
+                <input className="mail_address_form" type="text" defaultValue="" />
+            </form>
+            <div className="password">パスワード</div>
+            <form>
+                <input className="password_form" type="text" defaultValue="" />
+            </form>
+            <Link to="/ReceivedList">
+                <Fab variant="extended" className="login_button">
+                        ログイン
+                </Fab>
+            </Link>
+            <div className="new_register">新規会員登録はこちら</div>
+            <div className="line"></div>
         </>
+
     );
 };
 
